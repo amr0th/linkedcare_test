a1766ce0fe1d4566b4d10297b5571d18b6b9e8c7
amr0th <ricardo.n.henriques@gmail.com>
2016-04-03 19:10:30 +0100

    Update numbers_controller.rb

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -4,12 +4,8 @@ class NumbersController < ApplicationController
 	end
 
 	def submit
-
-		number_test = BusinessLogic::NumberTest.new({ values: [
-																														['linked', params[:numbers][:first_number].to_i], 
-																												   	['care', params[:numbers][:second_number].to_i]
-																								          ]
-																								})
+		number_test = BusinessLogic::NumberTest.new({ values: [ ['linked', params[:numbers][:first_number].to_i], 
+									['care', params[:numbers][:second_number].to_i] ] })
 		@results = number_test.perform
 		render "index"
 	end
